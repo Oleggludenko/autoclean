72c5035eabc039d5cddccbd866560f0f783c956d
Oleggludenko <odyxuaq@gmail.com>
2026-04-02 13:57:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function scrollToContact() {
     behavior: "smooth"
   });
 }
+// ===== Лінії за курсором =====
+const linesCount = 10;
+const lines = [];
+
+for (let i = 0; i < linesCount; i++) {
+  const line = document.createElement('div');
+  line.classList.add('cursor-line');
+  document.body.appendChild(line);
+  lines.push(line);
+}
+
+document.addEventListener('mousemove', (e) => {
+  lines.forEach((line, index) => {
+    const offset = index * 15; // відставання ліній
+    line.style.transform = `translate(${e.clientX - offset}px, ${e.clientY - offset}px)`;
+  });
+});
